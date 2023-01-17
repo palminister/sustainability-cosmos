3305a0211c571de2930d20f5c2fabd85d0cea26b
palminister <thanapatj.th@gmail.com>
2023-01-17 19:41:35 +0700

    Add no data legend

---
+++ src/components/dashboard/NullSizeLegend.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let width = 90;
+	export let height = 25;
+	let fill = "none";
+	let stroke = "#E5FF7B";
+	let r = 7;
+	let strokeWidth = 3;
+</script>
+
+<svg {width} {height}>
+	<g>
+		<circle
+			cx={r + strokeWidth}
+			cy={height - (r + strokeWidth)}
+			r={7}
+			{fill}
+			{stroke}
+			stroke-width={3}
+		/>
+		<text x={r + strokeWidth + 20} y={height - r}>no data</text>
+	</g>
+</svg>
+
+<style>
+	text {
+		font-size: var(--12px);
+		fill: var(--color-white);
+	}
+</style>

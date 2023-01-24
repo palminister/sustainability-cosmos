18e75eaa5ed319864c03fb630d924a07e38c071c
palminister <thanapatj.th@gmail.com>
2023-01-24 23:18:12 +0700

    Add navigation

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -3,13 +3,42 @@
 	import Demo from "$components/demo/Demo.svelte";
 	import WIP from "$components/helpers/WIP.svelte";
 	import Charts from "$components/dashboard/Charts.svelte";
+	import Landing from "$components/dashboard/Landing.svelte";
+	import { fade } from "svelte/transition";
+
 	// import Footer from "$components/Footer.svelte";
 
 	// const copy = getContext("copy");
 	// const data = getContext("data");
+
+	let renderOption = "";
+	const handleNavigation = (e) => {
+		renderOption = e.detail.e.target.innerText;
+	};
+	$: isTutorial = renderOption === "Tutorial";
 </script>
 
 <!-- <WIP /> -->
 <!-- <Demo /> -->
 <!-- <Footer /> -->
-<Charts />
+<div class="home">
+	{#if renderOption === ""}
+		<Landing on:navigate={handleNavigation} />
+	{:else}
+		<div transition:fade>
+			<Charts {isTutorial} />
+		</div>
+	{/if}
+</div>
+
+<!-- <Charts /> -->
+<style>
+	.home {
+		background: rgba(20, 23, 82, 1);
+		background: linear-gradient(
+			0deg,
+			rgba(16, 17, 39, 1) 0%,
+			rgba(20, 23, 82, 1) 100%
+		);
+	}
+</style>

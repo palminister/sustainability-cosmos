b4089a47ca68e8e7ddfe028b88a1fb3400469dad
palminister <thanapatj.th@gmail.com>
2023-01-27 01:52:53 +0700

    Add close warning option

--- src/components/helpers/MobileWarning.svelte
+++ src/components/helpers/MobileWarning.svelte
@@ -1,31 +1,49 @@
 <script>
 	let innerWidth = 1024;
 	let innerHeight = 600;
+	let close = false;
 	$: isDesktop = innerWidth >= 1024 && innerHeight >= 600;
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
-{#if !isDesktop}
-	<div>
-		🚧 This project is optimized for viewing on a desktop device. For the best
-		experience, we recommend using a computer or laptop. 🚧
+{#if !isDesktop && !close}
+	<div class="warning-container">
+		<div class="warning-prompt">
+			🚧 This project is optimized for viewing on a desktop device. For the best
+			experience, we recommend using a computer or laptop. <button
+				on:click={() => {
+					close = true;
+				}}>continue anyway</button
+			>🚧
+		</div>
 	</div>
 {/if}
 
 <style>
-	div {
+	.warning-container {
 		position: fixed;
 		z-index: 999999999;
 		top: 0;
 		left: 0;
 		width: 100%;
-		text-align: center;
+		height: 100%;
+	}
+	.warning-prompt {
 		padding: 1.25em;
+		text-align: center;
 		background: var(--color-yellow);
 		color: var(--color-purple-dark);
 		font-family: monospace;
-		font-size: 16px;
+		font-size: var(--14px);
 		animation: fadeIn 0.5s cubic-bezier(0.47, 0, 0.05, 1);
+	}
+	button {
+		padding: 0;
+		background: none;
+		font-family: monospace;
+		text-decoration: underline;
+		opacity: 0.5;
+		margin-right: 8px;
 	}
 </style>

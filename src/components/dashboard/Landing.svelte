90420dde356f305af6629ca1c6317d3f57a88b7a
palminister <thanapatj.th@gmail.com>
2023-01-28 02:23:39 +0700

    Update landing text

--- src/components/dashboard/Landing.svelte
+++ src/components/dashboard/Landing.svelte
@@ -29,7 +29,7 @@
 						Welcome to the cosmic realm of sustainability, where the stars align
 						to reveal the true nature of our world's progress. In this
 						interactive dashboard, we invite you to embark on a journey through
-						the galaxy of data, where each planet represents a country and its
+						the galaxy of data, where each star represents a country and its
 						position in the constellation reflects multiple things depending on
 						the dashboard views. As you navigate through the vast expanse of
 						information, you'll discover the secrets of each country's

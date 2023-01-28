de47f6d235b74b9ba9d3467bdba5778b2e49a3ee
palminister <thanapatj.th@gmail.com>
2023-01-28 20:52:18 +0700

    update github pages

---
+++ docs/_app/immutable/chunks/2-ae2a1baf.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-37d7fd68.js";const e=!0;export{t as component,e as server};
--- docs/_app/immutable/chunks/2-c89d6a0f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-7563e112.js";const e=!0;export{t as component,e as server};
